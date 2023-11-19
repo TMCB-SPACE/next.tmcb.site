ddcd1310488cac92ae0103bc29652478fd32b2cd
TED Vortex <ted.vortex@gmail.com>
2023-11-19 22:14:15 +0200

    fix: correct SSR rendering of theme toggle

--- components/shared/ThemeToggle.tsx
+++ components/shared/ThemeToggle.tsx
@@ -1,10 +1,20 @@
 'use client'
 
+import { useState, useEffect } from 'react'
 import { useTheme } from 'next-themes'
 import { DarkModeSwitch } from 'react-toggle-dark-mode';
 
 function ThemeToggle() {
+  const [mounted, setMounted] = useState(false)
   const { theme, setTheme } = useTheme()
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
 
   return (
     <DarkModeSwitch
