7410cbd102713af01c8cb4304f3babe5c4af0e27
TED Vortex <ted.vortex@gmail.com>
2023-11-20 19:17:50 +0200

    fix: correct text color and absolute border on generic page

--- components/pages/page/Page.tsx
+++ components/pages/page/Page.tsx
@@ -17,9 +17,8 @@ export function Page({ data }: PageProps) {
         <Header title={title} description={overview} />
 
         {/* Body */}
-        {body && <CustomPortableText paragraphClasses="font-serif max-w-3xl text-gray-600 text-xl" value={body} />}
+        {body && <CustomPortableText paragraphClasses="font-serif max-w-3xl text-xl" value={body} />}
       </div>
-      <div className="absolute left-0 w-screen border-t" />
     </div>
   )
 }
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -13,10 +13,10 @@ export function Header(props: HeaderProps) {
   return (
     <div className={`${centered ? 'text-center' : 'w-5/6 lg:w-3/5'}`}>
       {/* Title */}
-      {title && <div className="text-3xl font-extrabold tracking-tight md:text-5xl">{title}</div>}
+      {title && <h1 className="text-3xl font-extrabold tracking-tight md:text-5xl">{title}</h1>}
       {/* Description */}
       {description && (
-        <div className="mt-4 font-serif text-xl text-gray-600 md:text-2xl">
+        <div className="my-4 font-serif text-xl md:text-2xl">
           <CustomPortableText value={description} />
         </div>
       )}
