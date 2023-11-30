3fd310461bd8e954914fc10651cbddda38e04ff8
TED Vortex <ted.vortex@gmail.com>
2023-11-30 18:26:21 +0200

    fix: correct multiple theme providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { JetBrains_Mono, PT_Sans, Ubuntu } from 'next/font/google'
 import { ReactNode } from 'react'
 
 import { Providers } from '@/app/providers'
+import { ThemeProvider } from '@/components/shared/ThemeProvider'
 
 const serif = Ubuntu({
   variable: '--font-serif',
@@ -26,7 +27,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={`${mono.variable} ${sans.variable} ${serif.variable}`} suppressHydrationWarning>
       <body className="bg-neutral-200 dark:bg-neutral-700 text-black dark:text-stone-100 min-h-[100dvh]">
-        <Providers>{children}</Providers>
+        <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
   )
